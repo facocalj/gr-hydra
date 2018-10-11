717e702d0e55dfbeafcc278e8f8426d9b3f7dcea
Joao F. Santos <facocalj@tcd.ie>
2018-10-11 12:57:32 +0100

    Updated TX GRCs

--- grc_blocks/app/tx_02.py
+++ grc_blocks/app/tx_02.py
@@ -3,20 +3,9 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Tx 02
-# Generated: Tue Oct  2 22:36:54 2018
+# Generated: Thu Oct 11 12:39:17 2018
 ##################################################
 
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
-
-from PyQt4 import Qt
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
@@ -25,34 +14,13 @@ from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
 import hydra
-import sys
 import threading
 
 
-class tx_02(gr.top_block, Qt.QWidget):
+class tx_02(gr.top_block):
 
-    def __init__(self, freq_2=1.0995e9, samp_rate_2=1e6):
+    def __init__(self, freq_2=1100500000, samp_rate_2=0.5e6):
         gr.top_block.__init__(self, "Tx 02")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Tx 02")
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "tx_02")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
         ##################################################
         # Parameters
@@ -63,8 +31,8 @@ class tx_02(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.hydra_gr_sink_0 = hydra.hydra_gr_client_sink(12, "192.168.5.182", 5000)
-        self.hydra_gr_sink_0.start_client(freq_2, samp_rate_2, 1024)
+        self.hydra_gr_sink_0 = hydra.hydra_gr_client_sink(12, '192.168.5.190', 5000)
+        self.hydra_gr_sink_0.start_client(freq_2, samp_rate_2, 64)
         self.digital_ofdm_tx_0 = digital.ofdm_tx(
         	  fft_len=64, cp_len=16,
         	  packet_length_tag_key="len",
@@ -88,12 +56,6 @@ class tx_02(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_vector_source_x_0_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))    
         self.connect((self.digital_ofdm_tx_0, 0), (self.blocks_throttle_0, 0))    
 
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "tx_02")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
-
-
     def get_freq_2(self):
         return self.freq_2
 
@@ -109,7 +71,7 @@ class tx_02(gr.top_block, Qt.QWidget):
 
 
 def argument_parser():
-    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
     return parser
 
 
@@ -117,21 +79,14 @@ def main(top_block_cls=tx_02, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
-    from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
     tb.start()
-    tb.show()
-
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
-    qapp.exec_()
+    try:
+        raw_input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
 
 
 if __name__ == '__main__':
